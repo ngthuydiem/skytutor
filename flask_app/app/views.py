81d073caf96403622c23ce5f0f3536fa915dc408
Kristin Nguyen <ngthuydiem@gmail.com>
2016-02-03 20:25:08 +0000

    Add main components for flask web app

---
+++ flask_app/app/views.py
@@ -0,0 +1,39 @@
+from flask import render_template
+from flask import request
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template("index.html")
+
+@app.route('/ask')
+def ask():
+	return render_template("ask.html")
+
+from gensim.models import word2vec
+model_name = "300features_40minwords_10context"
+model = word2vec.Word2Vec.load(model_name)
+#model = word2vec.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+
+@app.route('/reply', methods=['POST','GET'])
+def ask_gensim():
+	words = request.form['words']
+	wordlist = words.split()
+	if wordlist:
+		if len(wordlist) == 1:
+			word = wordlist[0]
+			try:
+				similar_words = model.most_similar(word)
+				result = ''
+				for word, similarity in similar_words:
+					if similarity >= 0.6:
+						result += word + ' '				
+				return '"%s" is most similar to "%s"' % (words,result)
+			except:
+				return 'I haven\'t learnt about "%s".' % (words)
+		else:
+			result = model.doesnt_match(wordlist)
+			return '"%s" is most different from "%s"' % (result,words)
+	else:
+		return 'Please ask!'
