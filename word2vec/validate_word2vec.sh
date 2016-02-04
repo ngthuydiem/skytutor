3dc57df2b5de89f504535a89eea2836c30c0aec7
Kristin Nguyen <ngthuydiem@gmail.com>
2016-02-04 19:37:01 +0000

    Add bash script for validation

---
+++ word2vec/validate_word2vec.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+        
+# %E - Elapsed real time (in [hours:]minutes:seconds)
+# %P - Percentage of the CPU that this job got, computed as (%U + %S) / %E i.e. CPU secs in user mode + CPU secs in system (kernel) mode / elapsed secs
+# %M - Maximum resident set size of the process during its lifetime, in Kbytes
+
+LOG_FILE="log_word2vec_validate.txt"
+
+declare -a algos=("skip-gram" "cbow")
+declare -a opts=("negative-sampling" "hierarchical-softmax")
+
+for algo in "${algos[@]}"
+do
+	for opt in "${opts[@]}"
+	do
+		input="text7"
+		echo "$input" "$algo" "$opt"
+		CMD="python word2vec_validate.py --model_file models/$input-$algo-$opt.model"
+		/usr/bin/time -f "Runtime:\t%E minutes\nCPU percentage:\t%P\nMax memory:\t%M KB" $CMD >>$LOG_FILE 2>&1
+	done   
+done       
