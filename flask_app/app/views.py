278bcc38b17221710297cdf06a4b9e7f20ef7162
Kristin Nguyen <ngthuydiem@gmail.com>
2016-02-11 05:11:49 +0000

    Add word2vec training with Tensorflow

--- flask_app/app/views.py
+++ flask_app/app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from flask import request
+from flask import Markup
 from app import app
+from parse import *
 
 @app.route('/')
 @app.route('/index')
@@ -12,28 +14,37 @@ def ask():
 	return render_template("ask.html")
 
 from gensim.models import word2vec
-model = word2vec.Word2Vec.load('/home/ubuntu/skytutor/word2vec/models/enwiki-skip-gram-negative-sampling.model')
+model = word2vec.Word2Vec.load('/home/ubuntu/skytutor/word2vec/models/enwik9_text-skip-gram-negative-sampling.model')
+#model = word2vec.Word2Vec.load('/home/ubuntu/skytutor/word2vec/models/enwiki-skip-gram-negative-sampling.model')
 #model = word2vec.Word2Vec.load('models/300features_40minwords_10context')
 #model = word2vec.Word2Vec.load_word2vec_format('models/GoogleNews-vectors-negative300.bin', binary=True)
 
 @app.route('/reply', methods=['POST','GET'])
 def ask_gensim():
-	words = request.form['words']
-	wordlist = words.split()
-	if wordlist:
-		if len(wordlist) == 1:
-			word = wordlist[0]
-			try:
-				similar_words = model.most_similar(word)
-				result = ''
-				for word, similarity in similar_words:
-					if similarity >= 0.6:
-						result += word + ' '				
-				return '"%s" is most similar to "%s"' % (words,result)
-			except:
-				return 'I haven\'t learnt about "%s".' % (words)
-		else:
-			result = model.doesnt_match(wordlist)
-			return '"%s" is most different from "%s"' % (result,words)
+	words = request.form['words']		
+	
+	if words.startswith("#"):
+		words = words[1:]		
+		result = model.doesnt_match(words.split())	
+		response = '"%s" is the most unrelated out of "%s"' % (result, words)
 	else:
-		return 'Please ask!'
+		try:		
+			pos = []
+			neg = []
+			for word in words.split():
+				if word[0] == '-':
+					neg.append(word[1:])
+				else:
+					pos.append(word)					
+			similar_words = model.most_similar(positive=pos, negative=neg)
+			result = ''
+			for word, similarity in similar_words:
+				if similarity >= 0.6:
+					result += '%s %.2f ' %(word, similarity)
+					#result += '%s ' %(word)
+			if not result:
+				result = str(similar_words[0])
+			response = '"%s" is/are most related to "%s"' % (words,result)
+		except:
+			response = 'I haven\'t learnt about "%s".' % (words)			
+	return "<h1>%s</h1>" % response
--- word2vec/train_word2vec.sh
+++ word2vec/train_word2vec.sh
@@ -3,17 +3,24 @@
 # %E - Elapsed real time (in [hours:]minutes:seconds)
 # %P - Percentage of the CPU that this job got, computed as (%U + %S) / %E i.e. CPU secs in user mode + CPU secs in system (kernel) mode / elapsed secs
 # %M - Maximum resident set size of the process during its lifetime, in Kbytes
+# python word2vec_optimized.py --train_data enwiki --eval_data questions-words.txt --save_path tf-models/
+
+# /usr/bin/time -f "Runtime:\t%E minutes\nCPU percentage:\t%P\nMax memory:\t%M KB" python word2vec_tensorflow.py --train_data enwiki --eval_data questions-words.txt --save_path tf-models/
+
+#/usr/bin/time -f "Runtime:\t%E minutes\nCPU percentage:\t%P\nMax memory:\t%M KB" python /home/ubuntu/tensorflow/tensorflow/models/embedding/word2vec_optimized.py --train_data /home/ubuntu/skytutor/word2vec/enwiki --eval_data /home/ubuntu/skytutor/word2vec/questions-words.txt --save_path /home/ubuntu/skytutor/word2vec/tf-models/
+
+#/usr/bin/time -f "Runtime:\t%E minutes\nCPU percentage:\t%P\nMax memory:\t%M KB" python /home/ubuntu/skytutor/word2vec/word2vec_tensorflow.py --train_data /home/ubuntu/skytutor/word2vec/enwik10 --eval_data /home/ubuntu/skytutor/word2vec/questions-words.txt --save_path /home/ubuntu/skytutor/word2vec/tf-models/
 
 LOG_FILE="log_word2vec_train.txt"
 
-declare -a algos=("skip-gram" "cbow")
-declare -a opts=("negative-sampling" "hierarchical-softmax")
+declare -a algos=("skip-gram") #"cbow")
+declare -a opts=("negative-sampling") #"hierarchical-softmax")
 
 for algo in "${algos[@]}"
 do
 	for opt in "${opts[@]}"
 	do
-		input="enwiki"
+		input="enwik9_text"
 		echo "$input" "$algo" "$opt"
 		CMD="python word2vec_gensim.py --input_file $input --output_file models/$input-$algo-$opt --nthreads 16 --training_algorithm $algo --optimization_technique $opt"
 		/usr/bin/time -f "Runtime:\t%E minutes\nCPU percentage:\t%P\nMax memory:\t%M KB" $CMD >>$LOG_FILE 2>&1
