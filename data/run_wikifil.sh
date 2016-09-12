af65097d6ebf5ecc38c5c5a87e9e8355634e89fa
Kristin Nguyen <ngthuydiem@hotmail.com>
2016-09-12 11:47:32 +0800

    Re-add models directory

--- data/run_wikifil.sh
+++ data/run_wikifil.sh
@@ -1,5 +1,5 @@
-input=viwiki-latest-pages-articles.xml # enwik8 enwik9 (frwiki,enwiki)viwiki-latest-pages-articles.xml 
-output=viwiki-cleaned # enwik8-cleaned enwik9-cleaned (frwiki,enwiki)viwiki-cleaned
+input=enwik8 # enwik8 enwik9 (frwiki,enwiki)viwiki-latest-pages-articles.xml 
+output=enwik8-cleaned # enwik8-cleaned enwik9-cleaned (frwiki,enwiki)viwiki-cleaned
 perl wikifil.pl $input > $output
 echo $input; head -c 500 $input; echo; ls -lh $input; echo;
 echo $output; head -c 100 $output; echo; ls -lh $output 
--- data/sample_data.sh
+++ data/sample_data.sh
@@ -1,5 +1,5 @@
 input="enwik8-cleaned"
-size_mb=1
+size_mb=10
 num_bytes=$(($size_mb * 1024 * 1024))
 output=$input"-"$size_mb"mb"
 echo $output
