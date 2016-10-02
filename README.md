bd09a5934ef2eee5c93705badc993d39ea33f3e1
Kristin Nguyen <ngthuydiem@hotmail.com>
2016-10-02 20:33:18 +0800

    Add draft file

--- README.md
+++ README.md
@@ -17,4 +17,3 @@
 - Automatic image captioning
 - Contextual search
 
-
--- data/test_tf_gpu_installation.sh
+++
@@ -1,2 +0,0 @@
-python -c 'import os; import inspect; import tensorflow; print(os.path.dirname(inspect.getfile(tensorflow)))'
-python -m tensorflow.models.image.mnist.convolutional
